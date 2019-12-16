6aa70f2e44582290ef806f5674eba29de37d66be
Juli <julia.kukulies@gu.se>
2019-12-16 17:29:36 +0100

    initiated

---
+++ parameters.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+# calculate wind directions
+
+# plot wind field
+
+# get vertical wind shear for location, level1,level2
+
+def vertical_windshear(day,lon_idx,lat_idx,upper,lower):
+    
+    
+
+
+# calculate vertical wind profile, given a grid box or input field 
+
+
+
+
+
+
+# calculate moisture flux convergence 
